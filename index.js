e727c5ec11bf2844aaaa3d923832c94d28af5e35
fahmiwazu <140903578+fahmiwazu@users.noreply.github.com>
2024-06-20 17:10:10 +0700

    feat: add comment for code review

--- index.js
+++ index.js
@@ -23,47 +23,58 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+// declare url link and id
 const oriurl = []
 const shortUrl = []
 
+// post new url service
 app.post('/api/shorturl', (req, res) => {
+
+  // insert url
   const url = req.body.url;
 
+  // url validation 
   if (!url.includes("https://") && !url.includes("http://")) {
     return res.json({ error: 'invalid url' });
   }
 
+  // url id declaration
   const urlID = oriurl.indexOf(url);
 
+  // store url into array and skip if url already existed
   if (urlID < 0) {
     oriurl.push(url);
     shortUrl.push(shortUrl.length);
 
     return res.json({
-      oriurl: url,
-      shorturl: shortUrl.length - 1
+      original_url: url,
+      short_url: shortUrl.length - 1
     });
   }
 
+  // return existing url
   return res.json({
-    oriurl: url,
-    shorturl: shortUrl[urlID]
+    original_url : url,
+    short_url : shortUrl[urlID]
   });
 
 
 });
 
-
+// redirect url service
 app.get('/api/shorturl/:shorturl', (req, res) => {
+  // declare shorturl id and parse into integer
   const shorturl = parseInt(req.params.shorturl);
   const urlID = shortUrl.indexOf(shorturl);
 
+  // validate url if not existed
   if (urlID < 0) {
     return req.json({
       error: "No short URL found for the given input"
     });
   }
 
+  // redirect into url destination
   res.redirect(oriurl[urlID]);
 
 });
